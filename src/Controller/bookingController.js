d50155a49505d2182d01870fe0af859838929fe3
tanishaa17 <krati1701@gmail.com>
2023-07-12 19:50:23 +0530

    updates error handeling

--- src/Controller/bookingController.js
+++ src/Controller/bookingController.js
@@ -6,15 +6,15 @@ bookingRouter.post("/book-seats", async (req, res) => {
     try {
         const seats = req.body;
         // console.log(seats);
-        if (seats > 7) return res.status(400).send({ message: "You can not book more than 7 seats!", success: false });
         if (!seats) return res.status(400).send({ message: "Please add the number of seats!", success: false });
+        // if (seats > 7) return res.status(400).send({ message: "You can not book more than 7 seats!", success: false });
         else {
             const newBookedSeat = new BookingSchema(seats);
             await newBookedSeat.save();
             return res.status(200).send(`Booking done successfully`)
         }
     } catch (error) {
-        res.status(500).send(`Error while booking seats`)
+        res.status(500).send(`Error while booking seats ${error}`)
         console.log(error);
     }
 })
