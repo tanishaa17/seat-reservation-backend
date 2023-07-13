0e87d2b314616dcd62c5ee23f9a01dad1269a9e0
tanishaa17 <krati1701@gmail.com>
2023-07-13 11:12:31 +0530

    updated  error msgs

--- src/Controller/bookingController.js
+++ src/Controller/bookingController.js
@@ -14,7 +14,7 @@ bookingRouter.post("/book-seats", async (req, res) => {
             return res.status(200).send(`Booking done successfully`)
         }
     } catch (error) {
-        // console.log(error);
+        console.log(error);
         return res.status(500).send(`Error while booking seats ${error}`)
     }
 })
